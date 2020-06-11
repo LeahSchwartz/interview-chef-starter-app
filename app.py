2bab0f9b751c2a3b636b80352a187685ed8e18a7
Leah Schwartz <leah.schwartz@duke.edu>
2020-06-10 22:18:35 -0400

    Clean up delete and post calls

--- app.py
+++ app.py
@@ -8,38 +8,31 @@ keeper = CuisineKeeper()
 
 @app.route('/cuisine-keeper/dishes/all', methods=['GET'])
 def all_dishes():
-    return jsonify({"dishes": keeper.get_dishes()})
+    return jsonify({"dishes": keeper.get_dishes()}), 200
 
 @app.route('/cuisine-keeper/dishes', methods=['POST', 'DELETE'])
 def new_dish():
-    if request.method == 'POST':
-        content = request.json
-        print(content['dish'])
-        result = {"response": "Success"}
-        if keeper.has_dish(content['dish']):
-            result["message"] = content['dish'] + " was already in your dishes!"
-        else:
-            keeper.add_dish(content['dish'])
-        return jsonify(result), 200
-    if request.method == 'DELETE':
-        content = request.json
-        print(content['dish'])
-        result = {"response": "Success"}
-        if not keeper.has_dish(content['dish']):
-            result["message"] = content['dish'] + " was not in your dishes!"
-        else:
-            keeper.remove_dish(content['dish'])
-        return jsonify(result), 200
-
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Cuisine Keeper</h1>
-<p>A prototype API for storing and retrieving all the best cuisine.</p>'''
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    content = request.json
+    code = 200
+    response = {"result": ""}
+    try:
+        dish = content['dish']
+        if request.method == 'POST':
+            if keeper.has_dish(dish):
+                response["result"] = dish + " was already in your dishes!"
+            else:
+                keeper.add_dish(dish)
+                response["result"] = dish + " was added to your dishes!"
+        if request.method == 'DELETE':
+            if not keeper.has_dish(dish):
+                response["result"] = dish + " was not in your dishes!"
+            else:
+                keeper.remove_dish(dish)
+                response["result"] = dish + " was removed from your dishes!"
+    except KeyError:
+        response['result'] = "You didn't include a dish!"
+        code = 400
+    return jsonify(response), code
 
 
 if __name__ == '__main__':
